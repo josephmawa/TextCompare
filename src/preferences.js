971c4e8ba191be4e9ddfc649777c9c86ee779f8f
Joseph Mawa <186199059+josephmawa@users.noreply.github.com>
2025-03-01 11:42:07 +0300

    chore: create  comparison token settings

--- src/preferences.js
+++ src/preferences.js
@@ -1,17 +1,27 @@
 import Adw from "gi://Adw";
 import GObject from "gi://GObject";
 import Gio from "gi://Gio";
+import Gtk from "gi://Gtk";
+
+import { comparisonTokens } from "./util.js";
 
 export const ComparePreferencesDialog = GObject.registerClass(
   {
     GTypeName: "ComparePreferencesDialog",
     Template: getResourceUri("preferences.ui"),
-    InternalChildren: ["system", "dark", "light"],
+    InternalChildren: ["system", "dark", "light", "comparison_token_settings"],
     Properties: {
       theme: GObject.ParamSpec.string(
         "theme",
         "Theme",
         "Preferred theme",
+        GObject.ParamFlags.READWRITE,
+        ""
+      ),
+      comparisonToken: GObject.ParamSpec.string(
+        "comparisonToken",
+        "comparison_token",
+        "Preferred comparison token",
         GObject.ParamFlags.READWRITE,
         ""
       ),
@@ -21,6 +31,8 @@ export const ComparePreferencesDialog = GObject.registerClass(
     constructor(options = {}) {
       super(options);
 
+      this.setComparisonTokenModel();
+
       this.settings = Gio.Settings.new(pkg.name);
       this.settings.bind(
         "preferred-theme",
@@ -28,7 +40,13 @@ export const ComparePreferencesDialog = GObject.registerClass(
         "theme",
         Gio.SettingsBindFlags.DEFAULT
       );
-    
+      this.settings.bind(
+        "comparison-token",
+        this,
+        "comparisonToken",
+        Gio.SettingsBindFlags.DEFAULT
+      );
+
       this.bind_property_full(
         "theme",
         this._system,
@@ -56,6 +74,70 @@ export const ComparePreferencesDialog = GObject.registerClass(
         (_, theme) => [theme, "dark"]
       );
 
+      this.bind_property_full(
+        "comparisonToken",
+        this._comparison_token_settings,
+        "selected",
+        GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE,
+        (_, token) => {
+          const comparisonToken = comparisonTokens.find(
+            ({ key }) => key === token
+          );
+
+          if (!comparisonToken) {
+            throw new Error(
+              "Mismatch between difficulty keys in the settings and in comparisonTokens array"
+            );
+          }
+
+          const model = this._comparison_token_settings.model;
+
+          for (let i = 0; i < model.get_n_items(); i++) {
+            const stringObject = model.get_item(i);
+
+            if (stringObject?.string === comparisonToken.description) {
+              return [true, i];
+            }
+          }
+          return [false, 0];
+        },
+        (_, selected) => {
+          const stringObject =
+            this._comparison_token_settings.model.get_item(selected);
+
+          if (stringObject?.string) {
+            const comparisonToken = comparisonTokens.find(
+              ({ description }) => description === stringObject?.string
+            );
+
+            if (!comparisonToken) {
+              throw new Error(
+                "There is a mismatch between comparison token descriptions in the settings model and comparisonTokens array"
+              );
+            }
+
+            return [true, comparisonToken.key];
+          }
+
+          return [false, "mixed"];
+        }
+      );
     }
+
+    setComparisonTokenModel = () => {
+      const _comparisonTokens = comparisonTokens.map(
+        ({ description }) => description
+      );
+      this._comparison_token_settings.model =
+        Gtk.StringList.new(_comparisonTokens);
+
+      const propExpression = Gtk.PropertyExpression.new(
+        Gtk.StringObject,
+        null,
+        "string"
+      );
+
+      this._comparison_token_settings.expression = propExpression;
+    };
   }
 );
---
+++ src/util/util.js
@@ -0,0 +1,18 @@
+export const comparisonTokens = [
+  {
+    key: "characters",
+    description: _("Characters"),
+  },
+  {
+    key: "words",
+    description: _("Words"),
+  },
+  {
+    key: "lines",
+    description: _("Lines"),
+  },
+  {
+    key: "sentences",
+    description: _("Sentences"),
+  },
+];
