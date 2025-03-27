89df92c07c473ddb470aef2ac08ed38f3e8e9a20
Joseph Mawa <186199059+josephmawa@users.noreply.github.com>
2025-03-27 17:42:31 +0300

    chore: add translators and minor refactor

--- src/about.js
+++ src/about.js
@@ -1,6 +1,9 @@
 import Adw from "gi://Adw?version=1";
 import Gtk from "gi://Gtk";
 
+const GITHUB_URL = "https://github.com/josephmawa/TextCompare";
+const translators = ["Heimen Stoffels", "albanobattistella", "John Peter Sa"];
+
 const aboutParams = {
   application_name: APP_NAME,
   developer_name: "Joseph Mawa",
@@ -9,12 +12,11 @@ const aboutParams = {
   license_type: Gtk.License.LGPL_3_0,
   developers: ["Joseph Mawa"],
   artists: ["Joseph Mawa"],
-  // Translators: Replace "translator-credits" with your name/username, and optionally an email or URL.
-  // translator_credits: _("translator-credits"),
+  translator_credits: translators.join("\n"),
   copyright: "Copyright © 2025 Joseph Mawa",
-  website: "https://github.com/josephmawa/TextCompare",
-  issue_url: "https://github.com/josephmawa/TextCompare/issues",
-  support_url: "https://github.com/josephmawa/TextCompare/issues"
+  website: GITHUB_URL,
+  issue_url: GITHUB_URL + "/issues",
+  support_url: GITHUB_URL + "/issues",
 };
 
 export const AboutDialog = () => {
